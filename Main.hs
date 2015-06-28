295dafd748cb259c6abdfa79a9a2c7a9d4035668
Pi Lanningham <QRebound@gmail.com>
2015-06-27 22:36:40 -0400

    Refactored a bit to move code towards cleaner style

--- Main.hs
+++ Main.hs
@@ -5,10 +5,10 @@ module Main where
 
 import           BasePrelude                (IO, Int, String, appendFile,
                                              filter, fmap, foldM, foldM_, head,
-                                             length, map, mapM, otherwise,
-                                             putStrLn, return, show, take,
-                                             undefined, writeFile, ($), (+),
-                                             (++), (.), (=<<))
+                                             length, map, mapM, mapM_,
+                                             otherwise, putStrLn, return, show,
+                                             take, undefined, writeFile, ($),
+                                             (+), (++), (.), (=<<))
 import           Control.Lens
 import           Data.Aeson
 import           Data.Aeson.Lens            (key, values, _String, _Value)
@@ -23,24 +23,35 @@ import           Data.Text.Encoding         (encodeUtf8)
 import           Debug.Trace
 import           Network.Wreq
 
-
+-- Secrets
 authToken :: IO ByteString
 authToken = readFile "auth_token.secret"
 
-bucketKey :: IO ByteString
-bucketKey = do
+bucketKeys :: IO [ByteString]
+bucketKeys = do
     contents <- readFile "bucket_key.secret"
-    return $ (head . lines) contents
+    return $ lines contents
 
+-- Constants
 rootUrl :: ByteString
 rootUrl = "https://api.runscope.com/buckets/"
 
+folder :: String
+folder = "requests/"
+
+maxRequests :: Int
+maxRequests = 1000
+-- Uri builders
 captureUrl :: ByteString -> ByteString
-captureUrl bucketKey = concat [rootUrl, bucketKey, "/captures"]
+captureUrl bucket = concat [rootUrl, bucket, "/captures"]
 
 messageUrl :: ByteString -> ByteString -> ByteString
-messageUrl bucketKey uuid = concat [rootUrl, bucketKey, "/messages/", uuid]
+messageUrl bucket uuid = concat [rootUrl, bucket, "/messages/", uuid]
 
+filename :: ByteString -> Int -> String
+filename eventType count = folder ++ unpack eventType ++ show count ++ ".json"
+
+-- Request helpers
 withToken :: ByteString -> Options -> Options
 withToken t o = o & header "Authorization" .~ [concat ["Bearer ", t]]
 
@@ -49,36 +60,36 @@ getAuth opts url = do
     token <- authToken
     getWith (withToken token opts) (unpack url)
 
+-- Lenses
+
 getData r = fromMaybe emptyArray $ r ^? responseBody . key "data" . _Value
 getBody r = fromMaybe "" $ getData r ^? key "request" . key "body" . _String
+getEventType v = fromMaybe "request" $ v ^? key "eventType" . _String
 
-getDetails :: ByteString -> IO T.Text
-getDetails uuid = do
-    bKey <- bucketKey
-    let url = messageUrl bKey uuid
+getDetails :: ByteString -> ByteString -> IO T.Text
+getDetails bucket uuid = do
+    let url = messageUrl bucket uuid
     response <- getAuth defaults url
     return $ getBody response
 
-folder :: String
-folder = "requests/"
-
-handleResponse :: Map ByteString Int -> T.Text -> IO (Map ByteString Int)
+handleResponse :: Map ByteString Int -> Value -> IO (Map ByteString Int)
 handleResponse counts v = do
-    let v' = fromMaybe emptyObject ((decode $ BL.fromStrict $ encodeUtf8 v) :: Maybe Value)
-    let reqType = encodeUtf8 $ fromMaybe "request" $ v' ^? key "eventType" . _String
-    let count = modify $ lookup reqType counts
-    let file = folder ++ unpack reqType ++ show count ++ ".json"
+    let eventType = encodeUtf8 $ getEventType v
+    let count = modify $ lookup eventType counts
+    let file = filename eventType count
     putStrLn $ "Writing " ++ file
-    writeFile file $ (BL.unpack . encode) v'
-    let newCounts = alter (Just . modify) reqType counts
+    writeFile file $ (BL.unpack . encode) v
+    let newCounts = alter (Just . modify) eventType counts
     return newCounts
     where
         modify a = fromMaybe 0 a + 1
 
-handleUUID :: Map ByteString Int -> ByteString -> IO (Map ByteString Int)
-handleUUID counts uuid = do
-    deets <- getDetails uuid
-    handleResponse counts deets
+handleUUID :: ByteString -> Map ByteString Int -> ByteString -> IO (Map ByteString Int)
+handleUUID bucket counts uuid = do
+    details <- getDetails bucket uuid
+    let maybeDecoded = (decode . BL.fromStrict . encodeUtf8) details :: Maybe Value
+    let decoded = fromMaybe emptyObject maybeDecoded
+    handleResponse counts decoded
 
 parseUUID encoded =
     let json = fromJSON encoded
@@ -86,14 +97,20 @@ parseUUID encoded =
         Error err -> Nothing
         Success a -> Just $ pack a
 
-main = do
-    putStrLn "Start..."
-    url <- fmap captureUrl bucketKey
-    let opts = defaults & param "count" .~ ["1000"]
+handleBucket :: Options -> ByteString -> IO ()
+handleBucket opts bucket = do
+    let url = captureUrl bucket
     response <- getAuth opts url
-    let body = getData response
-    let uuids = body ^.. values . key "uuid"
+    let responseData = getData response
+    let uuids = responseData ^.. values . key "uuid"
     let extracted = mapMaybe parseUUID uuids
     putStrLn $ "Processing " ++ show (length extracted) ++ " uuids."
-    foldM_ handleUUID empty extracted
+    foldM_ (handleUUID bucket) empty extracted
+
+main :: IO ()
+main = do
+    putStrLn "Start..."
+    buckets <- bucketKeys
+    let opts = defaults & param "count" .~ [(T.pack . show) maxRequests]
+    mapM_ (handleBucket opts) buckets
     putStrLn "Done"
